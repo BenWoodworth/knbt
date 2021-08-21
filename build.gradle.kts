25dc04f01bc3edcf7bb5c8dbbe69d7a7013078ad
Ben Woodworth <ben@benwoodworth.net>
2021-08-21 16:28:48 -0400

    Update dokka to v1.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     kotlin("multiplatform") version "1.5.0"
     kotlin("plugin.serialization") version "1.5.0"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.4.0"
-    id("org.jetbrains.dokka") version "1.4.32"
+    id("org.jetbrains.dokka") version "1.5.0"
     id("maven-publish")
     id("signing")
 }
