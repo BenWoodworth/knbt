83055c640d127385a5266a0b12c6e80b631d33ea
Ben Woodworth <ben@benwoodworth.net>
2021-09-13 01:02:54 -0400

    Remove deprecations introduced in v0.9.0

--- src/commonMain/kotlin/NbtConfiguration.kt
+++ src/commonMain/kotlin/NbtConfiguration.kt
@@ -7,17 +7,6 @@ public class NbtConfiguration internal constructor(
     override val encodeDefaults: Boolean,
     override val ignoreUnknownKeys: Boolean,
 ) : NbtFormatConfiguration {
-    @Suppress("DeprecatedCallableAddReplaceWith")
-    @Deprecated("Use StringifiedNbt instead", level = DeprecationLevel.ERROR)
-    public val prettyPrint: Boolean
-        get() = error("Use StringifiedNbt instead")
-
-    @Suppress("DeprecatedCallableAddReplaceWith")
-    @Deprecated("Use StringifiedNbt instead", level = DeprecationLevel.ERROR)
-    @ExperimentalNbtApi
-    public val prettyPrintIndent: String
-        get() = error("Use StringifiedNbt instead")
-
     @OptIn(ExperimentalNbtApi::class)
     override fun toString(): String =
         "NbtConfiguration(" +
