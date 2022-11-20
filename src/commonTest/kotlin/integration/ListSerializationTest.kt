06f59e928c178af700024370e1df7f8afe55d639
Ben Woodworth <ben@benwoodworth.net>
2022-11-20 00:07:54 -0500

    Add integration tests for List and Array serialization

--- src/commonTest/kotlin/integration/ListSerializationTest.kt
+++ src/commonTest/kotlin/integration/ListSerializationTest.kt
@@ -1,9 +1,18 @@
 package net.benwoodworth.knbt.integration
 
 import net.benwoodworth.knbt.*
+import net.benwoodworth.knbt.test.serializers.SurrogateSerializer
 import kotlin.test.Test
 
 class ListSerializationTest : SerializationTest() {
+    @Test
+    fun should_serialize_List_to_NbtList() {
+        defaultNbt.testSerialization(
+            listOf(1, 2, 3),
+            NbtList(listOf(1, 2, 3).map(::NbtInt)),
+        )
+    }
+
     @Test
     fun should_serialize_List_of_Lists_correctly() {
         defaultNbt.testSerialization(
@@ -12,6 +21,65 @@ class ListSerializationTest : SerializationTest() {
                 addNbtList<NbtByte> { add(1.toByte()) }
                 addNbtList<Nothing> { }
             },
+        )
+    }
+
+
+    @Test
+    fun should_serialize_ByteArray_to_NbtByteArray() {
+        defaultNbt.testSerialization(
+            byteArrayOf(1, 2, 3),
+            NbtByteArray(byteArrayOf(1, 2, 3)),
+            ByteArray::contentEquals
+        )
+    }
+
+    @Test
+    fun should_serialize_IntArray_to_NbtIntArray() {
+        defaultNbt.testSerialization(
+            intArrayOf(1, 2, 3),
+            NbtIntArray(intArrayOf(1, 2, 3)),
+            IntArray::contentEquals
+        )
+    }
+
+    @Test
+    fun should_serialize_LongArray_to_NbtLongArray() {
+        defaultNbt.testSerialization(
+            longArrayOf(1L, 2L, 3L),
+            NbtLongArray(longArrayOf(1L, 2L, 3L)),
+            LongArray::contentEquals
+        )
+    }
+
+
+    @Test
+    fun should_serialize_ByteArray_to_NbtList_with_list_serializer() {
+        defaultNbt.testSerialization(
+            SurrogateSerializer(ByteArray::toList, List<Byte>::toByteArray),
+            byteArrayOf(1, 2, 3),
+            NbtList(listOf<Byte>(1, 2, 3).map(::NbtByte)),
+            ByteArray::contentEquals
+        )
+    }
+
+    @Test
+    fun should_serialize_IntArray_to_NbtList_with_list_serializer() {
+        defaultNbt.testSerialization(
+            SurrogateSerializer(IntArray::toList, List<Int>::toIntArray),
+            intArrayOf(1, 2, 3),
+            NbtList(listOf(1, 2, 3).map(::NbtInt)),
+            IntArray::contentEquals
+        )
+    }
+
+    @Test
+    fun should_serialize_LongArray_to_NbtList_with_list_serializer() {
+        defaultNbt.testSerialization(
+            SurrogateSerializer(LongArray::toList, List<Long>::toLongArray),
+            longArrayOf(1L, 2L, 3L),
+            NbtList(listOf(1L, 2L, 3L).map(::NbtLong)),
+            LongArray::contentEquals
         )
     }
 }
