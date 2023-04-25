102240e96fe31391e153d45d0e11b9b1a6ac384b
Ben Woodworth <ben@benwoodworth.net>
2023-04-24 20:06:47 -0400

    Adapt `JsonContentPolymorphicSerializer` for NBT
    Resolves #17

--- src/commonMain/kotlin/NbtContentPolymorphicSerializer.kt
+++ src/commonMain/kotlin/NbtContentPolymorphicSerializer.kt
@@ -1,8 +1,10 @@
 /*
+ * NOTICE: Modified from `JsonContentPolymorphicSerializer` in kotlinx.serialization v1.5.0.
+ *
  * Copyright 2017-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package kotlinx.serialization.json
+package net.benwoodworth.knbt
 
 import kotlinx.serialization.*
 import kotlinx.serialization.descriptors.*
@@ -15,21 +17,21 @@ import kotlin.reflect.*
  * without a dedicated class discriminator, on a content basis.
  *
  * Usually, polymorphic serialization (represented by [PolymorphicSerializer] and [SealedClassSerializer])
- * requires a dedicated `"type"` property in the JSON to
+ * requires a dedicated `"type"` property in the NBT to
  * determine actual serializer that is used to deserialize Kotlin class.
  *
- * However, sometimes (e.g. when interacting with external API) type property is not present in the input
- * and it is expected to guess the actual type by the shape of JSON, for example by the presence of specific key.
- * [JsonContentPolymorphicSerializer] provides a skeleton implementation for such strategy. Please note that
- * since JSON content is represented by [JsonElement] class and could be read only with [JsonDecoder] decoder,
- * this class works only with [Json] format.
+ * However, sometimes (e.g. when reading NBT data from Minecraft) type property is not present in the input
+ * and it is expected to guess the actual type by the shape of NBT, for example by the presence of specific key.
+ * [NbtContentPolymorphicSerializer] provides a skeleton implementation for such strategy. Please note that
+ * since NBT content is represented by [NbtTag] class and could be read only with [NbtDecoder] decoder,
+ * this class works only with [NBT format][NbtFormat].
  *
- * Deserialization happens in two stages: first, a value from the input JSON is read
- * to as a [JsonElement]. Second, [selectDeserializer] function is called to determine which serializer should be used.
- * The returned serializer is used to deserialize [JsonElement] back to Kotlin object.
+ * Deserialization happens in two stages: first, a value from the input NBT is read
+ * to as a [NbtTag]. Second, [selectDeserializer] function is called to determine which serializer should be used.
+ * The returned serializer is used to deserialize [NbtTag] back to Kotlin object.
  *
  * It is possible to serialize values this serializer. In that case, class discriminator property won't
- * be added to JSON stream, i.e., deserializing a class from the string and serializing it back yields the original string.
+ * be added to NBT stream, i.e., deserializing a class from the string and serializing it back yields the original string.
  * However, to determine a serializer, a standard polymorphic mechanism represented by [SerializersModule] is used.
  * For convenience, [serialize] method can lookup default serializer, but it is recommended to follow
  * standard procedure with [registering][SerializersModuleBuilder.polymorphic].
@@ -46,35 +48,37 @@ import kotlin.reflect.*
  * @Serializable
  * data class RefundedPayment(override val amount: String, val date: String, val reason: String) : Payment
  *
- * object PaymentSerializer : JsonContentPolymorphicSerializer<Payment>(Payment::class) {
- *     override fun selectDeserializer(content: JsonElement) = when {
- *         "reason" in content.jsonObject -> RefundedPayment.serializer()
+ * object PaymentSerializer : NbtContentPolymorphicSerializer<Payment>(Payment::class) {
+ *     override fun selectDeserializer(tag: NbtTag) = when {
+ *         "reason" in tag.nbtCompound -> RefundedPayment.serializer()
  *         else -> SuccessfulPayment.serializer()
  *     }
  * }
  *
  * // Now both statements will yield different subclasses of Payment:
  *
- * Json.parse(PaymentSerializer, """{"amount":"1.0","date":"03.02.2020"}""")
- * Json.parse(PaymentSerializer, """{"amount":"2.0","date":"03.02.2020","reason":"complaint"}""")
+ * StringifiedNbt.decodeFromString(PaymentSerializer, """{"amount":"1.0","date":"03.02.2020"}""")
+ * StringifiedNbt.decodeFromString(PaymentSerializer, """{"amount":"2.0","date":"03.02.2020","reason":"complaint"}""")
  * ```
  *
  * @param T A root class for all classes that could be possibly encountered during serialization and deserialization.
  * @param baseClass A class token for [T].
  */
 @OptIn(ExperimentalSerializationApi::class)
-public abstract class JsonContentPolymorphicSerializer<T : Any>(private val baseClass: KClass<T>) : KSerializer<T> {
+public abstract class NbtContentPolymorphicSerializer<T : Any>(private val baseClass: KClass<T>) : KSerializer<T> {
     /**
      * A descriptor for this set of content-based serializers.
      * By default, it uses the name composed of [baseClass] simple name,
      * kind is set to [PolymorphicKind.SEALED] and contains 0 elements.
      *
-     * However, this descriptor can be overridden to achieve better representation of custom transformed JSON shape
+     * However, this descriptor can be overridden to achieve better representation of custom transformed NBT shape
      * for schema generating/introspection purposes.
      */
+    @OptIn(InternalSerializationApi::class)
     override val descriptor: SerialDescriptor =
-        buildSerialDescriptor("JsonContentPolymorphicSerializer<${baseClass.simpleName}>", PolymorphicKind.SEALED)
+        buildSerialDescriptor("NbtContentPolymorphicSerializer<${baseClass.simpleName}>", PolymorphicKind.SEALED)
 
+    @OptIn(InternalSerializationApi::class)
     final override fun serialize(encoder: Encoder, value: T) {
         val actualSerializer =
             encoder.serializersModule.getPolymorphic(baseClass, value)
@@ -85,18 +89,17 @@ public abstract class JsonContentPolymorphicSerializer<T : Any>(private val base
     }
 
     final override fun deserialize(decoder: Decoder): T {
-        val input = decoder.asJsonDecoder()
-        val tree = input.decodeJsonElement()
+        val input = decoder.asNbtDecoder()
+        val tree = input.decodeNbtTag()
 
-        @Suppress("UNCHECKED_CAST")
         val actualSerializer = selectDeserializer(tree) as KSerializer<T>
-        return input.json.decodeFromJsonElement(actualSerializer, tree)
+        return input.nbt.decodeFromNbtTag(actualSerializer, tree)
     }
 
     /**
-     * Determines a particular strategy for deserialization by looking on a parsed JSON [element].
+     * Determines a particular strategy for deserialization by looking on a parsed NBT [tag].
      */
-    protected abstract fun selectDeserializer(element: JsonElement): DeserializationStrategy<T>
+    protected abstract fun selectDeserializer(tag: NbtTag): DeserializationStrategy<T>
 
     private fun throwSubtypeNotRegistered(subClass: KClass<*>, baseClass: KClass<*>): Nothing {
         val subClassName = subClass.simpleName ?: "$subClass"
--- src/commonTest/kotlin/serialization/NbtContentPolymorphicSerializationTest.kt
+++ src/commonTest/kotlin/serialization/NbtContentPolymorphicSerializationTest.kt
@@ -1,18 +1,18 @@
 /*
+ * NOTICE: Modified from `JsonContentPolymorphicSerializerTest` in kotlinx.serialization v1.5.0.
+ *
  * Copyright 2017-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package kotlinx.serialization.json.polymorphic
+package net.benwoodworth.knbt.serialization
 
 import kotlinx.serialization.*
-import kotlinx.serialization.json.*
+import net.benwoodworth.knbt.*
 import kotlin.test.*
 
-class JsonContentPolymorphicSerializerTest : JsonTestBase() {
-    val json = Json
-
+class NbtContentPolymorphicSerializationTest : SerializationTest() {
     @Serializable
-    sealed class Choices {
+    private sealed class Choices {
         @Serializable
         data class HasA(val a: String) : Choices()
 
@@ -23,9 +23,9 @@ class JsonContentPolymorphicSerializerTest : JsonTestBase() {
         data class HasC(val c: Boolean) : Choices()
     }
 
-    object ChoicesParametricSerializer : JsonContentPolymorphicSerializer<Choices>(Choices::class) {
-        override fun selectDeserializer(element: JsonElement): KSerializer<out Choices> {
-            val obj = element.jsonObject
+    private object ChoicesParametricSerializer : NbtContentPolymorphicSerializer<Choices>(Choices::class) {
+        override fun selectDeserializer(tag: NbtTag): KSerializer<out Choices> {
+            val obj = tag.nbtCompound
             return when {
                 "a" in obj -> Choices.HasA.serializer()
                 "b" in obj -> Choices.HasB.serializer()
@@ -36,7 +36,7 @@ class JsonContentPolymorphicSerializerTest : JsonTestBase() {
     }
 
     @Serializable
-    data class WithChoices(@Serializable(ChoicesParametricSerializer::class) val response: Choices)
+    private data class WithChoices(@Serializable(ChoicesParametricSerializer::class) val response: Choices)
 
     private val testDataInput = listOf(
         """{"response":{"a":"string"}}""",
@@ -51,53 +51,55 @@ class JsonContentPolymorphicSerializerTest : JsonTestBase() {
     )
 
     @Test
-    fun testParsesParametrically() = parametrizedTest { streaming ->
+    fun testParsesParametrically() {
         for (i in testDataInput.indices) {
-            assertEquals(
-                testDataOutput[i],
-                json.decodeFromString(WithChoices.serializer(), testDataInput[i], streaming),
-                "failed test on ${testDataInput[i]}, jsonTestingMode = $streaming"
+            defaultNbt.testDecoding(
+                WithChoices.serializer(),
+                StringifiedNbt.decodeFromString(testDataInput[i]),
+                testDataOutput[i]
             )
         }
     }
 
     @Test
-    fun testSerializesParametrically() = parametrizedTest { streaming ->
+    fun testSerializesParametrically() {
         for (i in testDataOutput.indices) {
-            assertEquals(
-                testDataInput[i],
-                json.encodeToString(WithChoices.serializer(), testDataOutput[i], streaming),
-                "failed test on ${testDataOutput[i]}, jsonTestingMode = $streaming"
+            defaultNbt.testEncoding(
+                WithChoices.serializer(),
+                testDataOutput[i],
+                StringifiedNbt.decodeFromString(testDataInput[i])
             )
         }
     }
 
-    interface Payment {
+    private interface Payment {
         val amount: String
     }
 
     @Serializable
-    data class SuccessfulPayment(override val amount: String, val date: String) : Payment
+    private data class SuccessfulPayment(override val amount: String, val date: String) : Payment
 
     @Serializable
-    data class RefundedPayment(override val amount: String, val date: String, val reason: String) : Payment
+    private data class RefundedPayment(override val amount: String, val date: String, val reason: String) : Payment
 
-    object PaymentSerializer : JsonContentPolymorphicSerializer<Payment>(Payment::class) {
-        override fun selectDeserializer(element: JsonElement) = when {
-            "reason" in element.jsonObject -> RefundedPayment.serializer()
+    private object PaymentSerializer : NbtContentPolymorphicSerializer<Payment>(Payment::class) {
+        override fun selectDeserializer(tag: NbtTag) = when {
+            "reason" in tag.nbtCompound -> RefundedPayment.serializer()
             else -> SuccessfulPayment.serializer()
         }
     }
 
     @Test
-    fun testDocumentationSample() = parametrizedTest { streaming ->
-        assertEquals(
+    fun testDocumentationSample() {
+        defaultNbt.testDecoding(
+            PaymentSerializer,
+            StringifiedNbt.decodeFromString("""{"amount":"1.0","date":"03.02.2020"}"""),
             SuccessfulPayment("1.0", "03.02.2020"),
-            json.decodeFromString(PaymentSerializer, """{"amount":"1.0","date":"03.02.2020"}""", streaming)
         )
-        assertEquals(
+        defaultNbt.testDecoding(
+            PaymentSerializer,
+            StringifiedNbt.decodeFromString("""{"amount":"2.0","date":"03.02.2020","reason":"complaint"}"""),
             RefundedPayment("2.0", "03.02.2020", "complaint"),
-            json.decodeFromString(PaymentSerializer, """{"amount":"2.0","date":"03.02.2020","reason":"complaint"}""", streaming)
         )
     }
 }
