a827fdf6495a96afc577ca0210ee3ce5def0c547
Ben Woodworth <ben@benwoodworth.com>
2024-05-31 19:58:27 -0400

    Update the `encodeToNbtTag` and `decodeFromNbtTag` docs
    Now matches the equivalent functions for `JsonElement`s in
    kotlinx.serialization.

--- src/commonMain/kotlin/NbtFormat.kt
+++ src/commonMain/kotlin/NbtFormat.kt
@@ -8,7 +8,9 @@ public sealed interface NbtFormat : SerialFormat {
     public val configuration: NbtFormatConfiguration
 
     /**
-     * Serializes and encodes the given [value] to an [NbtTag] using the given [serializer].
+     * Serializes the given [value] into an equivalent [NbtTag] using the given [serializer].
+     *
+     * @throws [SerializationException] if the given value cannot be serialized to NBT
      */
     public fun <T> encodeToNbtTag(serializer: SerializationStrategy<T>, value: T): NbtTag {
         lateinit var result: NbtTag
@@ -17,7 +19,10 @@ public sealed interface NbtFormat : SerialFormat {
     }
 
     /**
-     * Decodes and deserializes the given [tag] to a value of type [T] using the given [deserializer].
+     * Deserializes the given [tag] into a value of type [T] using the given [deserializer].
+     *
+     * @throws [SerializationException] if the given NBT tag is not a valid NBT input for the type [T]
+     * @throws [IllegalArgumentException] if the decoded input cannot be represented as a valid instance of type [T]
      */
     public fun <T> decodeFromNbtTag(deserializer: DeserializationStrategy<T>, tag: NbtTag): T =
         decodeFromNbtReader(TreeNbtReader(tag), deserializer)
@@ -44,15 +49,19 @@ public sealed interface NbtFormatBuilder {
 }
 
 /**
- * Serializes and encodes the given [value] to an [NbtTag] using
- * serializer retrieved from the reified type parameter.
+ * Serializes the given [value] into an equivalent [NbtTag] using a serializer retrieved from the reified type
+ * parameter.
+ *
+ * @throws [SerializationException] if the given value cannot be serialized to NBT
  */
 public inline fun <reified T> NbtFormat.encodeToNbtTag(value: T): NbtTag =
     encodeToNbtTag(serializersModule.serializer(), value)
 
 /**
- * Decodes and deserializes the given [tag] to a value of type [T] using
- * serializer retrieved from the reified type parameter.
+ * Deserializes the given [tag] into a value of type [T] using a serializer retrieved from the reified type parameter.
+ *
+ * @throws [SerializationException] if the given NBT tag is not a valid NBT input for the type [T]
+ * @throws [IllegalArgumentException] if the decoded input cannot be represented as a valid instance of type [T]
  */
 public inline fun <reified T> NbtFormat.decodeFromNbtTag(tag: NbtTag): T =
     decodeFromNbtTag(serializersModule.serializer(), tag)
