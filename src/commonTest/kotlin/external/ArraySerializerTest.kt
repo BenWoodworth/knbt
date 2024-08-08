69d723b8b158d9a9a1e613535d6754bb5eefcb49
Ben Woodworth <ben@benwoodworth.com>
2024-08-08 13:01:40 -0400

    Parameterize `ArraySerializer` test suite
    The same test was essentially duplicated 6 times, once for each
    specially supported array type, which makes it easy to simplify the test
    suite.
    
    There will also be additional tests added based on these test cases, so
    this refactor paves the way for that too.

--- src/commonTest/kotlin/external/ArraySerializerTest.kt
+++ src/commonTest/kotlin/external/ArraySerializerTest.kt
@@ -1,100 +1,83 @@
 package net.benwoodworth.knbt.external
 
+import com.benwoodworth.parameterize.parameter
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.builtins.*
 import net.benwoodworth.knbt.NbtByteArray
 import net.benwoodworth.knbt.NbtIntArray
 import net.benwoodworth.knbt.NbtLongArray
+import net.benwoodworth.knbt.NbtTag
 import net.benwoodworth.knbt.test.parameterizeTest
 import net.benwoodworth.knbt.test.parameters.parameterOfVerifyingNbt
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 
 class ArraySerializerTest {
-    @Test
-    fun should_serialize_ByteArray_correctly() = parameterizeTest {
-        val nbt by parameterOfVerifyingNbt()
+    private class ArraySerializerTestCase<T>(
+        val serializer: KSerializer<T>,
+        val array: T,
+        val tag: NbtTag,
+        val assertContentEquals: (expected: T, actual: T) -> Unit
+    ) {
+        override fun toString() = serializer.descriptor.toString()
+    }
 
-        nbt.verifyEncoderOrDecoder(
+    @OptIn(ExperimentalUnsignedTypes::class, ExperimentalSerializationApi::class)
+    private val arraySerializerTestCases = listOf(
+        ArraySerializerTestCase(
             ByteArraySerializer(),
             byteArrayOf(1, 2, 3),
             NbtByteArray(listOf(1, 2, 3)),
-            testDecodedValue = { value, decodedValue ->
-                assertContentEquals(value, decodedValue)
-            }
-        )
-    }
-
-    @Test
-    fun should_serialize_IntArray_correctly() = parameterizeTest {
-        val nbt by parameterOfVerifyingNbt()
-
-        nbt.verifyEncoderOrDecoder(
+            ::assertContentEquals
+        ),
+        ArraySerializerTestCase(
             IntArraySerializer(),
             intArrayOf(1, 2, 3),
             NbtIntArray(listOf(1, 2, 3)),
-            testDecodedValue = { value, decodedValue ->
-                assertContentEquals(value, decodedValue)
-            }
-        )
-    }
-
-    @Test
-    fun should_serialize_LongArray_correctly() = parameterizeTest {
-        val nbt by parameterOfVerifyingNbt()
-
-        nbt.verifyEncoderOrDecoder(
+            ::assertContentEquals
+        ),
+        ArraySerializerTestCase(
             LongArraySerializer(),
             longArrayOf(1, 2, 3),
             NbtLongArray(listOf(1, 2, 3)),
-            testDecodedValue = { value, decodedValue ->
-                assertContentEquals(value, decodedValue)
-            }
-        )
-    }
-
-    @Test
-    @OptIn(ExperimentalUnsignedTypes::class, ExperimentalSerializationApi::class)
-    fun should_serialize_UByteArray_correctly() = parameterizeTest {
-        val nbt by parameterOfVerifyingNbt()
-
-        nbt.verifyEncoderOrDecoder(
+            ::assertContentEquals
+        ),
+        ArraySerializerTestCase(
             UByteArraySerializer(),
             ubyteArrayOf(1u, 2u, 3u),
             NbtByteArray(listOf(1, 2, 3)),
-            testDecodedValue = { value, decodedValue ->
-                assertContentEquals(value, decodedValue)
-            }
-        )
-    }
-
-    @Test
-    @OptIn(ExperimentalUnsignedTypes::class, ExperimentalSerializationApi::class)
-    fun should_serialize_UIntArray_correctly() = parameterizeTest {
-        val nbt by parameterOfVerifyingNbt()
-
-        nbt.verifyEncoderOrDecoder(
+            ::assertContentEquals
+        ),
+        ArraySerializerTestCase(
             UIntArraySerializer(),
             uintArrayOf(1u, 2u, 3u),
             NbtIntArray(listOf(1, 2, 3)),
-            testDecodedValue = { value, decodedValue ->
-                assertContentEquals(value, decodedValue)
-            }
-        )
-    }
-
-    @Test
-    @OptIn(ExperimentalUnsignedTypes::class, ExperimentalSerializationApi::class)
-    fun should_serialize_ULongArray_correctly() = parameterizeTest {
-        val nbt by parameterOfVerifyingNbt()
-
-        nbt.verifyEncoderOrDecoder(
+            ::assertContentEquals
+        ),
+        ArraySerializerTestCase(
             ULongArraySerializer(),
             ulongArrayOf(1u, 2u, 3u),
             NbtLongArray(listOf(1, 2, 3)),
-            testDecodedValue = { value, decodedValue ->
-                assertContentEquals(value, decodedValue)
-            }
-        )
+            ::assertContentEquals
+        ),
+    )
+
+    @Test
+    fun should_serialize_array_correctly() = parameterizeTest {
+        val nbt by parameterOfVerifyingNbt()
+        val arraySerializer by parameter(arraySerializerTestCases)
+
+        fun <T> ArraySerializerTestCase<T>.test() {
+            nbt.verifyEncoderOrDecoder(
+                serializer,
+                array,
+                tag,
+                testDecodedValue = { value, decodedValue ->
+                    assertContentEquals(value, decodedValue)
+                }
+            )
+        }
+        arraySerializer.test() // KT-68606
     }
 }
