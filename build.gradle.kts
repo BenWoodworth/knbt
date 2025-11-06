999d11709dd2443d84e2fc9beb9c5318598ee5e2
Ben Woodworth <ben@benwoodworth.com>
2025-11-05 22:48:32 -0500

    Migrate to Central Publisher Portal publication
    The OSSRH service has been sunset, so migrate to the new Central
    Publisher Portal. There's no officially Gradle plugin that supports the
    new Publisher API yet, so this uses the Portal OSSRH Staging API as a
    stopgap for now.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+import groovy.json.JsonSlurper
 import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import java.util.Base64
 
 val kotlinx_serialization_version: String by extra
 val okio_version: String by extra
@@ -138,16 +144,19 @@ signing {
     }
 }
 
+val mavenCentralPortalUsername: String? = System.getenv("MAVEN_CENTRAL_PORTAL_USERNAME")
+val mavenCentralPortalPassword: String? = System.getenv("MAVEN_CENTRAL_PORTAL_PASSWORD")
+
 publishing {
     repositories {
         maven {
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            url = if (isSnapshot) snapshotsRepoUrl else releasesRepoUrl
+            val releasesUrl = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
+            val snapshotsUrl = uri("https://central.sonatype.com/repository/maven-snapshots/")
+            url = if (isSnapshot) snapshotsUrl else releasesUrl
 
             credentials {
-                username = System.getenv("OSSRH_USERNAME")
-                password = System.getenv("OSSRH_TOKEN")
+                username = mavenCentralPortalUsername
+                password = mavenCentralPortalPassword
             }
         }
     }
@@ -178,6 +187,81 @@ publishing {
                 developerConnection = "scm:git:ssh://github.com:BenWoodworth/knbt.git"
                 url = "https://github.com/BenWoodworth/knbt"
             }
+        }
+    }
+}
+
+/**
+ * OSSRH Staging API: [`POST /manual/upload/defaultRepository`](https://ossrh-staging-api.central.sonatype.com/swagger-ui/#/default/manual_upload_default_repository)
+ *
+ * This endpoint is intended to support the usecase of publishing to OSSRH via the "Maven style" PUT-based API. That API
+ * does not open a staging repository before sending files, so OSSRH's behavior was to create a default repository based
+ * on the files that were uploaded. Publishers will need to call this endpoint in order to make the deployment available
+ * in the Portal for their manual testing.
+ *
+ * @param namespace The namespace of the repository. Can be found by visiting https://central.sonatype.com/publishing/namespaces.
+ */
+fun ossrhStagingApiUpload(namespace: String, username: String, password: String): OssrhStagingApiUploadResult {
+    val baseUrl = "https://ossrh-staging-api.central.sonatype.com"
+    val namespaceUrlParameter = URLEncoder.encode(namespace, StandardCharsets.UTF_8.name())
+    val url = URL("$baseUrl/manual/upload/defaultRepository/$namespaceUrlParameter")
+
+    val token = Base64.getEncoder().encodeToString("$username:$password".encodeToByteArray())
+
+    logger.debug("POST {}", url)
+    val connection = (url.openConnection() as HttpURLConnection).apply {
+        requestMethod = "POST"
+        setRequestProperty("Authorization", "Bearer $token")
+    }
+
+    try {
+        logger.debug("Response: ${connection.responseMessage}")
+
+        return when (connection.responseCode) {
+            HttpURLConnection.HTTP_BAD_REQUEST -> {
+                // Response schema: { "error": "string" }
+                val response = connection.errorStream.use { JsonSlurper().parse(it) }
+                check(response is Map<*, *>) { "Response is not an object. Got ${response::class.simpleName}." }
+
+                val errorEntry = response.entries.firstOrNull { it.key == "error" }
+                checkNotNull(errorEntry) { "Response object did not contain 'error'." }
+
+                val error = errorEntry.value
+                check(error is String) { "Response error was not a string. Got ${error?.javaClass?.simpleName}." }
+
+                OssrhStagingApiUploadResult.FailedUpload(error)
+            }
+
+            HttpURLConnection.HTTP_OK -> OssrhStagingApiUploadResult.SuccessfulUpload
+            HttpURLConnection.HTTP_UNAUTHORIZED -> OssrhStagingApiUploadResult.Unauthorized
+            else -> error("Unhandled response: ${connection.responseMessage}")
+        }
+    } finally {
+        connection.disconnect()
+    }
+}
+
+sealed interface OssrhStagingApiUploadResult {
+    object SuccessfulUpload : OssrhStagingApiUploadResult
+    data class FailedUpload(val error: String) : OssrhStagingApiUploadResult
+    object Unauthorized : OssrhStagingApiUploadResult
+}
+
+tasks.register("uploadOssrhStagingApiDeployment") {
+    group = "publishing"
+    description = "Upload OSSRH Staging API deployment to the Central Publisher Portal"
+    onlyIf("Only needed for releases, which use the Central Publishing Portal") { !isSnapshot }
+
+    doLast {
+        val namespace = project.group.toString()
+        val username = checkNotNull(mavenCentralPortalUsername) { "Publishing username is not set" }
+        val password = checkNotNull(mavenCentralPortalPassword) { "Publishing password is not set" }
+
+        logger.lifecycle("Uploading OSSRH Staging API deployment to Central Publisher Portal")
+        when (val result = ossrhStagingApiUpload(namespace, username, password)) {
+            OssrhStagingApiUploadResult.SuccessfulUpload -> logger.lifecycle("Successful upload")
+            is OssrhStagingApiUploadResult.FailedUpload -> throw GradleException("Failed upload: ${result.error}")
+            OssrhStagingApiUploadResult.Unauthorized -> throw GradleException("Unable to upload: Unauthorized")
         }
     }
 }
