ff915657d956eb1438355e40d14dccbeb870a9d8
Ben Woodworth <ben@benwoodworth.com>
2025-11-19 20:32:44 -0500

    Update Parameterize to 0.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ kotlin {
             dependencies {
                 implementation(kotlin("test"))
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinx_serialization_version")
-                implementation("com.benwoodworth.parameterize:parameterize:0.4.0")
+                implementation("com.benwoodworth.parameterize:parameterize-core:0.4.1")
             }
         }
         val jvmTest by getting {
